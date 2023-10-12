4e7f3143c264b7f2f8899dc19292a1b6089d9c1a
Arjunreddypreethi <arjunreddypreethi1999@gmail.com>
2023-10-12 18:56:14 +0530

    loading animation illustration {gsap}

---
+++ project-2_loading-animation/static/js/main.js
@@ -0,0 +1,24 @@
+gsap.to('.loader .loading.one',{
+  y:-100,
+  duration:0.5,
+  repeat:-1,
+  yoyo:true,
+})
+gsap.to('.loader .loading.two',{
+  y:-100,
+  duration:0.51,
+  repeat:-1,
+  yoyo:true,
+})
+gsap.to('.loader .loading.three',{
+  y:-100,
+  duration:0.52,
+  repeat:-1,
+  yoyo:true,
+})
+gsap.to('.loader .loading.four',{
+  y:-100,
+  duration:0.53,
+  repeat:-1,
+  yoyo:true,
+})
